518ff04757cfe4c1a09a138d31a2305a844571af
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-15 19:46:52 +0200

    Initial commit

---
+++ src/main/java/pl/kmiecik/m2_homework_shop/M2HomeWorkShopApplication.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.m2_homework_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2HomeWorkShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2HomeWorkShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kmiecik/m2_homework_shop/M2HomeWorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.m2_homework_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2HomeWorkShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
